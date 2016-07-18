a332831fd5b571439a9106972f854d44cf8dbd12
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2016-07-18 12:36:50 -0700

    download/save: save downloaded urls as files
    Signed-off-by: Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>

---
+++ download/save.go
@@ -0,0 +1,30 @@
+package download
+
+import "os"
+import "github.com/pkg/errors"
+import "io"
+
+const (
+	writeBufSize = 1024 * 2014
+)
+
+// SaveTo uses given downloader to fetch the resource with retries and saves the
+// given file. Directory of dst is not created by this function. If a file at
+// dst exists, it will be truncated. If a new file is created, mode is used to
+// set the permission bits. Written number of bytes are returned on success.
+func SaveTo(d Downloader, dst string, mode os.FileMode) (int64, error) {
+	f, err := os.OpenFile(dst, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, mode)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to open file for writing")
+	}
+	defer f.Close()
+
+	body, err := WithRetries(d, ActualSleep)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to download file")
+	}
+	defer body.Close()
+
+	n, err := io.CopyBuffer(f, body, make([]byte, writeBufSize))
+	return n, errors.Wrapf(err, "failed to write to file: %s", dst)
+}
---
+++ download/save_test.go
@@ -0,0 +1,84 @@
+package download_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/Azure/custom-script-extension-linux/download"
+	"github.com/ahmetalpbalkan/go-httpbin"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSaveTo_invalidDir(t *testing.T) {
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	d := download.NewURLDownload(srv.URL + "/bytes/65536")
+
+	_, err := download.SaveTo(d, "/nonexistent-dir/dst", 0600)
+	require.Contains(t, err.Error(), "failed to open file for writing")
+}
+
+func TestSave(t *testing.T) {
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	dir, err := ioutil.TempDir("", "")
+	require.Nil(t, err)
+	defer os.RemoveAll(dir)
+
+	d := download.NewURLDownload(srv.URL + "/bytes/65536")
+	path := filepath.Join(dir, "test-file")
+	n, err := download.SaveTo(d, path, 0600)
+	require.Nil(t, err)
+	require.EqualValues(t, 65536, n)
+
+	fi, err := os.Stat(path)
+	require.Nil(t, err)
+	require.EqualValues(t, 65536, fi.Size())
+	require.Equal(t, os.FileMode(0600), fi.Mode().Perm(), "not chmod'ed")
+}
+
+func TestSave_truncates(t *testing.T) {
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	dir, err := ioutil.TempDir("", "")
+	require.Nil(t, err)
+	defer os.RemoveAll(dir)
+
+	path := filepath.Join(dir, "test-file")
+	_, err = download.SaveTo(download.NewURLDownload(srv.URL+"/bytes/65536"), path, 0600)
+	require.Nil(t, err)
+	_, err = download.SaveTo(download.NewURLDownload(srv.URL+"/bytes/128"), path, 0777)
+	require.Nil(t, err)
+
+	fi, err := os.Stat(path)
+	require.Nil(t, err)
+	require.EqualValues(t, 128, fi.Size())
+	require.Equal(t, os.FileMode(0600), fi.Mode().Perm(), "mode should not be changed")
+}
+
+func TestSave_largeFile(t *testing.T) {
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	dir, err := ioutil.TempDir("", "")
+	require.Nil(t, err)
+	defer os.RemoveAll(dir)
+
+	size := 1024 * 1024 * 128 // 128 mb
+
+	path := filepath.Join(dir, "large-file")
+	n, err := download.SaveTo(download.NewURLDownload(srv.URL+"/bytes/"+fmt.Sprintf("%d", size)), path, 0600)
+	require.Nil(t, err)
+	require.EqualValues(t, size, n)
+
+	fi, err := os.Stat(path)
+	require.Nil(t, err)
+	require.EqualValues(t, size, fi.Size())
+}
