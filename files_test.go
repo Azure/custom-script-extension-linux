fc82b71168271eaf9271f9900e42bd99b9f8d3c9
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2016-07-19 09:37:32 -0700

    Process file urls
    These methods will help download a file from URL,
    save it to a directory with proper name and apply
    post-download/pre-execution processing (DOS, BOM etc)
    on it.
    
    Signed-off-by: Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>

---
+++ files_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_getDownloader_azureBlob(t *testing.T) {
+	// error condition
+	_, err := getDownloader("http://acct.blob.core.windows.net/", "acct", "key")
+	require.NotNil(t, err)
+
+	// valid input
+	d, err := getDownloader("http://acct.blob.core.windows.net/container/blob", "acct", "key")
+	require.Nil(t, err)
+	require.NotNil(t, d)
+	require.Equal(t, "download.blobDownload", fmt.Sprintf("%T", d), "got wrong type")
+}
+
+func Test_getDownloader_externalUrl(t *testing.T) {
+	d, err := getDownloader("http://acct.blob.core.windows.net/", "", "")
+	require.Nil(t, err)
+	require.NotNil(t, d)
+	require.Equal(t, "download.urlDownload", fmt.Sprintf("%T", d), "got wrong type")
+}
+
+func Test_urlToFileName_badURL(t *testing.T) {
+	_, err := urlToFileName("http://192.168.0.%31/")
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), `unable to parse URL: "http://192.168.0.%31/"`)
+}
+
+func Test_urlToFileName_noFileName(t *testing.T) {
+	cases := []string{
+		"http://example.com",
+		"http://example.com",
+		"http://example.com/",
+		"http://example.com/#foo",
+		"http://example.com?bar",
+		"http://example.com/bar/",  // empty after last slash
+		"http://example.com/bar//", // empty after last slash
+		"http://example.com/?bar",
+		"http://example.com/?bar#quux",
+	}
+
+	for _, c := range cases {
+		_, err := urlToFileName(c)
+		require.NotNil(t, err, "not failed: %s", "url=%s", c)
+		require.Contains(t, err.Error(), "cannot extract file name from URL", "url=%s", c)
+	}
+}
+
+func Test_urlToFileName(t *testing.T) {
+	cases := []struct{ in, out string }{
+		{"http://example.com/1", "1"},
+		{"http://example.com/1/2", "2"},
+		{"http://example.com/1///2", "2"},
+		{"http://example.com/1/2?3=4", "2"},
+		{"http://example.com/1/2?3#", "2"},
+	}
+	for _, c := range cases {
+		fn, err := urlToFileName(c.in)
+		require.Nil(t, err, "url=%s")
+		require.Equal(t, c.out, fn, "url=%s", c)
+	}
+}
+
+func Test_postProcessFile_fail(t *testing.T) {
+	require.NotNil(t, postProcessFile("/non/existing/path"))
+}
+
+func Test_postProcessFile(t *testing.T) {
+	f, err := ioutil.TempFile("", "")
+	require.Nil(t, err)
+	defer os.RemoveAll(f.Name())
+	_, err = fmt.Fprintf(f, "#!/bin/sh\r\necho 'Hello, world!'\n")
+	require.Nil(t, err)
+	f.Close()
+
+	require.Nil(t, postProcessFile(f.Name()))
+
+	b, err := ioutil.ReadFile(f.Name())
+	require.Nil(t, err)
+	require.Equal(t, []byte("#!/bin/sh\necho 'Hello, world!'\n"), b)
+}
