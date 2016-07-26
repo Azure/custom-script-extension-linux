3cf0367488b6c63c424a97930ac4f7d463d119bd
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2016-07-26 11:52:07 -0700

    cmds: split enable() into several methods, add tests
    extracted logic in enable() to smaller and testable
    methods.

--- cmds.go
+++ cmds.go
@@ -47,6 +47,19 @@ func install(ctx *log.Context, h vmextension.HandlerEnvironment) error {
 	return nil
 }
 
+func uninstall(ctx *log.Context, h vmextension.HandlerEnvironment) error {
+	{ // a new context scope with path
+		ctx = ctx.With("path", dataDir)
+		ctx.Log("event", "removing data dir", "path", dataDir)
+		if err := os.RemoveAll(dataDir); err != nil {
+			return errors.Wrap(err, "failed to delete data dir")
+		}
+		ctx.Log("event", "removed data dir")
+	}
+	ctx.Log("event", "uninstalled")
+	return nil
+}
+
 func enable(ctx *log.Context, h vmextension.HandlerEnvironment) error {
 	// parse the extension handler settings (not available prior to 'enable')
 	cfg, err := parseAndValidateSettings(ctx, h.HandlerEnvironment.ConfigFolder)
@@ -54,32 +67,62 @@ func enable(ctx *log.Context, h vmextension.HandlerEnvironment) error {
 		return errors.Wrap(err, "failed to get configuration")
 	}
 
-	seq, err := strconv.Atoi(h.SeqNo)
-	if err != nil {
-		return errors.Wrapf(err, "failed to parse seqnum: %q", h.SeqNo)
-	}
-
-	// exit if this sequence number (a snapshot of the configuration) is
+	// exit if this sequence number (a snapshot of the configuration) is alrady
 	// processed. if not, save this sequence number before proceeding.
-	sf := filepath.Join(dataDir, seqNumFile)
-	ctx.Log("event", "comparing seqnum")
-	if b, err := seqnum.IsSmallerOrEqualThan(sf, seq); err != nil {
-		return errors.Wrap(err, "failed to check sequence number")
-	} else if b {
-		ctx.Log("event", "exit", "message", "this configuration is already processed, will not run again")
+	seqNumPath := filepath.Join(dataDir, seqNumFile)
+	if shouldExit, err := checkAndSaveSeqNum(ctx, h.SeqNo, seqNumPath); err != nil {
+		return errors.Wrap(err, "failed to process ")
+	} else if shouldExit {
+		ctx.Log("event", "exit", "message", "this script configuration is already processed, will not run again")
 		os.Exit(0) // exit immediately to prevent reporting '.status'
 	}
-	if err := seqnum.Set(sf, seq); err != nil {
-		return errors.Wrap(err, "failed to save sequence number")
-	}
-	ctx.Log("event", "seqnum saved", "path", sf)
 
-	// - prepare the output directory for files and the command output
 	dir := filepath.Join(dataDir, downloadDir, h.SeqNo)
+	if err := downloadFiles(ctx, dir, cfg); err != nil {
+		return errors.Wrap(err, "processing file downloads failed")
+	}
+
+	if err := runCmd(ctx, dir, cfg); err != nil {
+		return err
+	}
+
+	ctx.Log("event", "enabled")
+	return nil
+}
+
+// checkAndSaveSeqNum checks if the given seqNum is already processed
+// according to the specified seqNumFile and if so, returns true,
+// otherwise saves the given seqNum into seqNumFile returns false.
+func checkAndSaveSeqNum(ctx log.Logger, seqNum, seqNumFile string) (shouldExit bool, _ error) {
+	ctx.Log("event", "comparing seqnum", "path", seqNumFile)
+	seq, err := strconv.Atoi(seqNum)
+	if err != nil {
+		return false, errors.Wrapf(err, "failed to parse seqnum: %q", seqNum)
+	}
+
+	if b, err := seqnum.IsSmallerOrEqualThan(seqNumFile, seq); err != nil {
+		return false, errors.Wrap(err, "failed to check sequence number")
+	} else if b {
+		ctx.Log("message", "saved seqnum <= current seqnum")
+		return true, nil
+	}
+	if err := seqnum.Set(seqNumFile, seq); err != nil {
+		return false, errors.Wrap(err, "failed to save the sequence number")
+	}
+	ctx.Log("event", "seqnum saved", "path", seqNumFile)
+	return false, nil
+}
+
+// downloadFiles downloads the files specified in cfg into dir (creates if does
+// not exist) and takes storage credentials specified in cfg into account.
+func downloadFiles(ctx *log.Context, dir string, cfg handlerSettings) error {
+	// - prepare the output directory for files and the command output
+	// - create the directory if missing
+	ctx.Log("event", "creating output directory", "path", dir)
 	if err := os.MkdirAll(dir, 0700); err != nil {
 		return errors.Wrap(err, "failed to prepare output directory")
 	}
-	ctx.Log("event", "created output directory", "path", dir)
+	ctx.Log("event", "created output directory")
 
 	// - download files
 	ctx.Log("files", len(cfg.FileURLs))
@@ -92,8 +135,11 @@ func enable(ctx *log.Context, h vmextension.HandlerEnvironment) error {
 		}
 		ctx.Log("event", "download complete", "output", dir)
 	}
+	return nil
+}
 
-	// - run the script
+// runCmd runs the command (extracted from cfg) in the given dir (assumed to exist).
+func runCmd(ctx log.Logger, dir string, cfg handlerSettings) error {
 	ctx.Log("event", "executing command", "output", dir)
 	cmd := cfg.publicSettings.CommandToExecute
 	if cmd == "" {
@@ -104,19 +150,5 @@ func enable(ctx *log.Context, h vmextension.HandlerEnvironment) error {
 		return errors.Wrap(err, "failed to execute command")
 	}
 	ctx.Log("event", "executed command", "output", dir)
-	ctx.Log("event", "enabled")
-	return nil
-}
-
-func uninstall(ctx *log.Context, h vmextension.HandlerEnvironment) error {
-	{ // a new context scope with path
-		ctx = ctx.With("path", dataDir)
-		ctx.Log("event", "removing data dir", "path", dataDir)
-		if err := os.RemoveAll(dataDir); err != nil {
-			return errors.Wrap(err, "failed to delete data dir")
-		}
-		ctx.Log("event", "removed data dir")
-	}
-	ctx.Log("event", "uninstalled")
 	return nil
 }
--- cmds_test.go
+++ cmds_test.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/ahmetalpbalkan/go-httpbin"
+	"github.com/go-kit/kit/log"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,7 +24,7 @@ func Test_commandsExist(t *testing.T) {
 }
 
 func Test_commands_shouldReportStatus(t *testing.T) {
-	// certain extension invocations are supposed to write 'N.status' files and some do not.
+	// - certain extension invocations are supposed to write 'N.status' files and some do not.
 
 	// these subcommands should NOT report status
 	require.False(t, cmds["install"].shouldReportStatus, "install should not report status")
@@ -28,4 +34,102 @@ func Test_commands_shouldReportStatus(t *testing.T) {
 	require.True(t, cmds["enable"].shouldReportStatus, "enable should report status")
 	require.True(t, cmds["disable"].shouldReportStatus, "disable should report status")
 	require.True(t, cmds["update"].shouldReportStatus, "update should report status")
+}
+
+func Test_checkAndSaveSeqNum_fails(t *testing.T) {
+	// pass in invalid seqnum format
+	_, err := checkAndSaveSeqNum(log.NewNopLogger(), "not-an-int", "/un/used")
+	require.Nil(t, err)
+	require.Contains(t, err.Error(), `failed to parse seqnum: "not-an-int"`)
+}
+
+func Test_checkAndSaveSeqNum(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	require.Nil(t, err)
+	fp := filepath.Join(dir, "seqnum")
+	defer os.RemoveAll(dir)
+
+	nop := log.NewNopLogger()
+
+	// no sequence number, 0 comes in.
+	shouldExit, err := checkAndSaveSeqNum(nop, "0", fp)
+	require.Nil(t, err)
+	require.False(t, shouldExit)
+
+	// file=0, seq=0 comes in. (should exit)
+	shouldExit, err = checkAndSaveSeqNum(nop, "0", fp)
+	require.Nil(t, err)
+	require.True(t, shouldExit)
+
+	// file=0, seq=1 comes in.
+	shouldExit, err = checkAndSaveSeqNum(nop, "1", fp)
+	require.Nil(t, err)
+	require.False(t, shouldExit)
+
+	// file=1, seq=1 comes in. (should exit)
+	shouldExit, err = checkAndSaveSeqNum(nop, "1", fp)
+	require.Nil(t, err)
+	require.True(t, shouldExit)
+
+	// file=1, seq=0 comes in. (should exit)
+	shouldExit, err = checkAndSaveSeqNum(nop, "1", fp)
+	require.Nil(t, err)
+	require.True(t, shouldExit)
+}
+
+func Test_runCmd_success(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	require.Nil(t, err)
+	defer os.RemoveAll(dir)
+
+	require.Nil(t, runCmd(log.NewNopLogger(), dir, handlerSettings{
+		publicSettings: publicSettings{CommandToExecute: "date"},
+	}), "command should run successfully")
+
+	// check stdout stderr files
+	_, err = os.Stat(filepath.Join(dir, "stdout"))
+	require.Nil(t, err, "stdout should exist")
+	_, err = os.Stat(filepath.Join(dir, "stderr"))
+	require.Nil(t, err, "stderr should exist")
+}
+
+func Test_runCmd_fail(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	require.Nil(t, err)
+	defer os.RemoveAll(dir)
+
+	err = runCmd(log.NewNopLogger(), dir, handlerSettings{
+		publicSettings: publicSettings{CommandToExecute: "non-existing-cmd"},
+	})
+	require.NotNil(t, err, "command terminated with exit status")
+	require.Contains(t, err.Error(), "failed to execute command")
+}
+
+func Test_downloadFiles(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	require.Nil(t, err)
+	defer os.RemoveAll(dir)
+
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	err = downloadFiles(log.NewContext(log.NewNopLogger()),
+		dir,
+		handlerSettings{
+			publicSettings: publicSettings{
+				FileURLs: []string{
+					srv.URL + "/bytes/10",
+					srv.URL + "/bytes/100",
+					srv.URL + "/bytes/1000",
+				}},
+		})
+	require.Nil(t, err)
+
+	// check the files
+	f := []string{"10", "100", "1000"}
+	for _, fn := range f {
+		fp := filepath.Join(dir, fn)
+		_, err := os.Stat(fp)
+		require.Nil(t, err, "%s is missing from download dir", fp)
+	}
 }
