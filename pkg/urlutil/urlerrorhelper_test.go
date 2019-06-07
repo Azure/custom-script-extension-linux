03e4d1ae6c1e6a3b12a3a115325b2ac7e1069191
Bhaskar Brahma <bhbrahma@microsoft.com>
2019-06-07 13:14:00 -0700

    fixed typo in comment

--- pkg/urlutil/urlerrorhelper_test.go
+++ pkg/urlutil/urlerrorhelper_test.go
@@ -31,7 +31,7 @@ func Test_urlparse02(t *testing.T) {
 }
 
 func Test_urlparse03(t *testing.T) {
-	// error string where the uri isn' sparated by space with the rest of the error message
+	// error string where the uri isn't separated by space with the rest of the error message
 	errorString :=	`Gethttps://aaaaaaaa.blob.core.windows.net/nodeagentpackage-version9-0-0-381/Ubuntu-16.04/batch_config-ubuntu-16.04-1.5.9.tar.gz?sv=2018-03-28&sr=b&sig=a%secret%2Bsecret&st=2019-05-17T01%3A25%3A42Z&se=2021-05-24T01%3A25%3A42Z&sp=r:dial tcp 13.68.165.64:443:i/o timeout`
 	inputErr := fmt.Errorf("%s", errorString)
 	outputErr := RemoveUrlFromErr(inputErr)
