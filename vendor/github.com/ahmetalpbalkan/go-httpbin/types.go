de6c868a1d97c5110281d4ef296b1c20961f401d
bhaskar brahma <bhbrahma@microsoft.com>
2022-10-06 16:08:48 -0700

    fixing go get for go 1.10

---
+++ vendor/github.com/ahmetalpbalkan/go-httpbin/types.go
@@ -0,0 +1,32 @@
+package httpbin
+
+type ipResponse struct {
+	Origin string `json:"origin"`
+}
+
+type errorResponse struct {
+	Error errObj `json:"error"`
+}
+
+type errObj struct {
+	Message string `json:"message"`
+}
+
+type userAgentResponse struct {
+	UA string `json:"user-agent"`
+}
+
+type headersResponse struct {
+	Headers map[string]string `json:"headers"`
+}
+
+type cookiesResponse struct {
+	Cookies map[string]string `json:"cookies"`
+}
+
+type getResponse struct {
+	headersResponse
+	ipResponse
+	URL  string                 `json:"url"`
+	Args map[string]interface{} `json:"args"`
+}
